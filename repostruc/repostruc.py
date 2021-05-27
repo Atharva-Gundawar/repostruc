aa708e92dc16b899f6e5b1ec283942c7ca059f0e
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-27 10:52:29 +0530

    Doc : fn dsc

--- repostruc/repostruc.py
+++ repostruc/repostruc.py
@@ -15,9 +15,14 @@ ignore_files = ['.git']
 print('\n')
 
 def getTree(dir_path: Path, prefix: str='', ignore_files: list=ignore_files):
-    """A recursive generator, given a directory Path object
-    will yield a visual tree structure line by line
-    with each line prefixed by the same characters
+    """
+    A recursive generator function,
+    which yeilds a visual tree structure line by line.
+
+    @param dir_path: Path object of targeted directory 
+    @param prefix: Prefix for every level of the tree structure 
+    @param ignore_files: Files and directories to ignore
+
     """    
     contents = [path for path in dir_path.iterdir() if path.name not in ignore_files]
     pointers = [tee] * (len(contents) - 1) + [last]
@@ -28,6 +33,14 @@ def getTree(dir_path: Path, prefix: str='', ignore_files: list=ignore_files):
             yield from getTree(path, prefix=prefix+extension,ignore_files=ignore_files)
 
 def formatTree(dir_path: Path, dsc_spacing: int=4, base_spacing: int=2):
+    """
+    Adds spacing before and after every file.
+
+    @param dir_path: Path object of targeted directory.
+    @param dsc_spacing: Spacing between file and discription.
+    @param base_spacing: Spacing before file. 
+
+    """
     tree_contents = [line for line in getTree(Path().absolute())]
     max_string_length = max(tree_contents, key=len)
     tree_contents = [' '*base_spacing + line + ' '*int(dsc_spacing + len(max_string_length) - len(line)) + '<= DSC' for line in tree_contents]
