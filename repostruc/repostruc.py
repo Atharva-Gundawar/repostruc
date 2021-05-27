0dc34b87c3b7746d8ff6210978605504114f4c0a
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-27 10:42:39 +0530

    Feat : Format tree

--- repostruc/repostruc.py
+++ repostruc/repostruc.py
@@ -12,13 +12,14 @@ last =   '└── '
 # files to ignore:
 ignore_files = ['.git']
 
+print('\n')
 
 def getTree(dir_path: Path, prefix: str='', ignore_files: list=ignore_files):
     """A recursive generator, given a directory Path object
     will yield a visual tree structure line by line
     with each line prefixed by the same characters
     """    
-    contents = [path for path in dir_path.iterdir() if path.stem not in ignore_files]
+    contents = [path for path in dir_path.iterdir() if path.name not in ignore_files]
     pointers = [tee] * (len(contents) - 1) + [last]
     for pointer, path in zip(pointers, contents):
         yield prefix + pointer + path.name
@@ -26,5 +27,12 @@ def getTree(dir_path: Path, prefix: str='', ignore_files: list=ignore_files):
             extension = branch if pointer == tee else space 
             yield from getTree(path, prefix=prefix+extension,ignore_files=ignore_files)
 
-for line in getTree(Path().absolute()):
-    print(line)
+def formatTree(dir_path: Path, dsc_spacing: int=4, base_spacing: int=2):
+    tree_contents = [line for line in getTree(Path().absolute())]
+    max_string_length = max(tree_contents, key=len)
+    tree_contents = [' '*base_spacing + line + ' '*int(dsc_spacing + len(max_string_length) - len(line)) + '<= DSC' for line in tree_contents]
+    for i in tree_contents:
+        print(i)
+
+
+formatTree(Path().absolute())
